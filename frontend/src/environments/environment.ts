d803cb5e103f65da17a9aa6d365579d68ae2a1e0
isaac <aderonmuisaac@gmail.com>
2020-07-21 02:24:12 +0100

    chore: auth0 variables

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'udacity-fsnd-coffee-shop.us', // the auth0 domain prefix https://udacity-fsnd-coffee-shop.us.auth0.com/api/v2/
+    audience: 'drinks', // the audience set for the auth0 app
+    clientId: 'YOAi6W01Kb3TFhLBA7S5mZt1u2iy9N73', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
