a781706e8b3fe7d7ede155a1f74e4ef2c92f8e04
isaac <aderonmuisaac@gmail.com>
2020-07-23 06:51:33 +0100

    style: PEP8 code style refactor

--- backend/src/api.py
+++ backend/src/api.py
@@ -19,25 +19,32 @@ CORS(app)
 '''
 # db_drop_and_create_all()
 
-## ROUTES
+# ROUTES
 '''
 @TODO implement endpoint
     GET /drinks
         it should be a public endpoint
         it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+    returns status code 200 and json
+    {
+        "success": True,
+        "drinks": drinks
+    }
+    where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+
+
 @app.route('/drinks')
 def get_public_drinks():
     all_drinks = Drink.query.all()
     drinks = []
-    
+
     if len(all_drinks) == 0:
         return jsonify({
-        "status": 200,
-        "success": True,
-        "drinks": drinks
+            "status": 200,
+            "success": True,
+            "drinks": drinks
         }), 200
 
     for each_drink in all_drinks:
@@ -56,9 +63,16 @@ def get_public_drinks():
     GET /drinks-detail
         it should require the 'get:drinks-detail' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+    returns status code 200 and json
+    {
+        "success": True,
+        "drinks": drinks
+    }
+    where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+
+
 @app.route('/drinks-detail')
 @requires_auth('get:drinks-detail')
 def get_drinks_detail(jwt):
@@ -68,7 +82,7 @@ def get_drinks_detail(jwt):
     for each_drink in all_drinks:
         long_detail = each_drink.long()
         drinks.append(long_detail)
-    
+
     return jsonify({
         "status": 200,
         "success": True,
@@ -82,9 +96,16 @@ def get_drinks_detail(jwt):
         it should create a new row in the drinks table
         it should require the 'post:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
+    returns status code 200 and json
+    {
+        "success": True,
+        "drinks": drink
+    }
+    where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
+
+
 @app.route('/drinks', methods=['POST'])
 @requires_auth('post:drinks')
 def post_drinks(jwt):
@@ -93,7 +114,7 @@ def post_drinks(jwt):
         abort(422)
     if 'recipe' not in data:
         abort(422)
-        
+
     new_title = data.get('title')
     new_recipe = data.get('recipe')
 
@@ -106,10 +127,10 @@ def post_drinks(jwt):
     # recipeJSON = getJSONListFromObject(recipe)
 
     try:
-        new_drink = Drink(title = new_title, recipe = json.dumps(new_recipe))
+        new_drink = Drink(title=new_title, recipe=json.dumps(new_recipe))
         new_drink.insert()
         drink = new_drink.long()
-            
+
         return jsonify({
             "status": 200,
             "success": True,
@@ -117,6 +138,7 @@ def post_drinks(jwt):
         }), 200
     except Exception:
         abort(422)
+
 
 '''
 @TODO implement endpoint
@@ -126,16 +148,23 @@ def post_drinks(jwt):
         it should update the corresponding row for <id>
         it should require the 'patch:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
+    returns status code 200 and json
+    {
+        "success": True,
+        "drinks": drink
+    }
+    where drink an array containing only the updated drink
         or appropriate status code indicating reason for failure
 '''
+
+
 @app.route('/drinks/<id>', methods=['PATCH'])
 @requires_auth('patch:drinks')
 def patch_drinks(jwt, id):
     update_drink = Drink.query.get(id)
     if update_drink is None:
         abort(404)
-    
+
     data = request.get_json()
     if "title" in data:
         update_drink.title = data.get('title')
@@ -143,7 +172,7 @@ def patch_drinks(jwt, id):
     if "recipe" in data:
         recipe = data.get('recipe')
         update_drink.recipe = recipe
-    
+
     try:
         update_drink.update()
         drink = []
@@ -156,6 +185,7 @@ def patch_drinks(jwt, id):
     except Exception:
         abort(422)
 
+
 '''
 @TODO implement endpoint
     DELETE /drinks/<id>
@@ -163,16 +193,23 @@ def patch_drinks(jwt, id):
         it should respond with a 404 error if <id> is not found
         it should delete the corresponding row for <id>
         it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
+    returns status code 200 and json
+    {
+        "success": True,
+        "delete": id
+    }
+    where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
+
+
 @app.route('/drinks/<id>', methods=['DELETE'])
 @requires_auth('delete:drinks')
 def delete_drinks(jwt, id):
     delete_drink = Drink.query.get(id)
     if delete_drink is None:
         abort(404)
-    
+
     id = delete_drink.id
     try:
         delete_drink.delete()
@@ -185,33 +222,39 @@ def delete_drinks(jwt, id):
         abort(422)
 
 
-## Error Handling
+# Error Handling
 '''
 Example error handling for unprocessable entity
 '''
+
+
 @app.errorhandler(422)
 def unprocessable(error):
     return jsonify({
-                    "success": False, 
+                    "success": False,
                     "error": 422,
                     "message": "unprocessable"
                     }), 422
+
 
 '''
 @TODO implement error handlers using the @app.errorhandler(error) decorator
     each error handler should return (with approprate messages):
              jsonify({
-                    "success": False, 
+                    "success": False,
                     "error": 404,
                     "message": "resource not found"
                     }), 404
 
 '''
 
+
 '''
 @TODO implement error handler for 404
     error handler should conform to general task above
 '''
+
+
 @app.errorhandler(400)
 def bad_request(error):
     return jsonify({
@@ -220,6 +263,7 @@ def bad_request(error):
         'message': 'Bad request'
     }), 400
 
+
 @app.errorhandler(401)
 def unauthorized(error):
     return jsonify({
@@ -227,6 +271,7 @@ def unauthorized(error):
         'error': 401,
         'message': 'Unauthorized'
     }), 401
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -239,15 +284,17 @@ def not_found(error):
 
 '''
 @TODO implement error handler for AuthError
-    error handler should conform to general task above 
+    error handler should conform to general task above
 '''
+
+
 @app.errorhandler(AuthError)
 def processAuthError(error):
     message = [str(x) for x in error.args]
     status_code = error.status_code
 
     return jsonify({
-                    "success": False, 
+                    "success": False,
                     "error": status_code,
                     "message": message
                     }), status_code
--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -9,18 +9,20 @@ AUTH0_DOMAIN = 'udacity-fsnd-coffee-shop.us.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'drinks'
 
-## AuthError Exception
+# AuthError Exception
 '''
 AuthError Exception
 A standardized way to communicate auth failure modes
 '''
+
+
 class AuthError(Exception):
     def __init__(self, error, status_code):
         self.error = error
         self.status_code = status_code
 
 
-## Auth Header
+# Auth Header
 
 '''
 @TODO implement get_token_auth_header() method
@@ -32,13 +34,15 @@ class AuthError(Exception):
 
     code source: https://github.com/udacity/FSND/tree/master/BasicFlaskAuth
 '''
+
+
 def get_token_auth_header():
     if "Authorization" not in request.headers:
         raise AuthError({
             'code': 'authorization_header_missing',
             'description': 'Authorization header is expected.'
         }, 401)
-    
+
     auth_header = request.headers['Authorization']
     header_parts = auth_header.split(' ')
 
@@ -63,6 +67,7 @@ def get_token_auth_header():
     token = header_parts[1]
     return token
 
+
 '''
 @TODO implement check_permissions(permission, payload) method
     @INPUTS
@@ -71,16 +76,20 @@ def get_token_auth_header():
 
     it should raise an AuthError if permissions are not included in the payload
         !!NOTE check your RBAC settings in Auth0
-    it should raise an AuthError if the requested permission string is not in the payload permissions array
+    it should raise an AuthError
+    if the requested permission string is not in the
+    payload permissions array
     return true otherwise
-    
+
 '''
+
+
 def check_permissions(permission, payload):
     if 'permissions' not in payload:
-                        raise AuthError({
-                            'code': 'invalid_claims',
-                            'description': 'Permissions not included in JWT.'
-                        }, 400)
+        raise AuthError({
+            'code': 'invalid_claims',
+            'description': 'Permissions not included in JWT.'
+        }, 400)
 
     if permission not in payload['permissions']:
         raise AuthError({
@@ -89,6 +98,7 @@ def check_permissions(permission, payload):
         }, 401)
 
     return True
+
 
 '''
 @TODO implement verify_decode_jwt(token) method
@@ -103,8 +113,11 @@ def check_permissions(permission, payload):
 
     code source: https://github.com/udacity/FSND/tree/master/BasicFlaskAuth
 
-    !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
+    !!NOTE urlopen has a common certificate error described here:
+    https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
 '''
+
+
 def verify_decode_jwt(token):
     jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
     jwks = json.loads(jsonurl.read())
@@ -158,6 +171,7 @@ def verify_decode_jwt(token):
                 'description': 'Unable to find the appropriate key.'
             }, 400)
 
+
 '''
 @TODO implement @requires_auth(permission) decorator method
     @INPUTS
@@ -165,9 +179,13 @@ def verify_decode_jwt(token):
 
     it should use the get_token_auth_header method to get the token
     it should use the verify_decode_jwt method to decode the jwt
-    it should use the check_permissions method validate claims and check the requested permission
-    return the decorator which passes the decoded payload to the decorated method
+    it should use the check_permissions method validate claims
+    and check the requested permission
+    return the decorator which passes the
+    decoded payload to the decorated method
 '''
+
+
 def requires_auth(permission=''):
     def requires_auth_decorator(f):
         @wraps(f)
@@ -175,9 +193,9 @@ def requires_auth(permission=''):
             token = get_token_auth_header()
             try:
                 payload = verify_decode_jwt(token)
-            except:
+            except Exception:
                 abort(401)
-            
+
             check_permissions(permission, payload)
             return f(payload, *args, **kwargs)
 
